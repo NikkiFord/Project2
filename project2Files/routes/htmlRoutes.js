4b433c0029f1189528aa39c2678f7675836813b4
Nikki Ford <nikkigford@gmail.com>
2019-10-22 17:28:01 -0600

    add initial code for wizard

--- project2Files/routes/htmlRoutes.js
+++ project2Files/routes/htmlRoutes.js
@@ -1,27 +1,31 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/", function (req, res) {
+    res.render("index", {
+      pageTitle: "Home"
     });
   });
+
+  app.get("/wizard-steps/:step", (req, res) => {
+    const step = req.params.step;
+    res.render(`wizard-steps/${step}`);
+  })
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  // // Render 404 page for any unmatched routes
+  // app.get("*", function(req, res) {
+  //   res.render("404");
+  // });
 };
+
+
