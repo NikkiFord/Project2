795067cac888410ea18ab5bc4bc4e7eeea57ee9f
Nikki Ford <nikkigford@gmail.com>
2019-10-21 19:43:19 -0600

    add post test file

--- project2Files/models/schema.sql
+++ project2Files/models/schema.sql
@@ -1,4 +1,34 @@
 DROP DATABASE IF EXISTS toDoList;
 CREATE DATABASE toDoList;
 
-
+INSERT INTO ToDoLists (item_name, category)
+VALUES
+(toothbrush, essentials),
+(toothpaste, essentials),
+(deodorant, essentials),
+(shampoo/conditioner, essentials),
+(dental floss, essentials),
+(Q-Tips, essentials),
+(underwear, essentials),
+(sunscreen, hot),
+(swimming suit, hot),
+(sandals, hot),
+(sunglasses, hot),
+(shorts, hot),
+(light jacket, warm),
+(closed toe shoes, warm),
+(long pants, warm),
+(short sleeved shirts),
+(boots, cold),
+(coat, cold),
+(scarf, cold),
+(hat, cold),
+(gloves, cold),
+(long sleeved shirts, cold),
+(heavy coat, colder),
+(scarf, colder),
+(hat, colder),
+(gloves, colder),
+(long sleeved shirts, colder),
+(hand warmers, colder),
+(snow pants, colder)
